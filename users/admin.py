e40139898547560108acfc16f72bfb49aa23b184
黎义 <liyi@forchange.tech>
2019-04-13 00:40:56 +0800

    feat: 在admin注册users app的模型

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import User
+from .models import User, EmailVerifyCode, Banner
 
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EmailVerifyCode)
+class EmailVerifyCodeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
     pass
--- users/models.py
+++ users/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import random, datetime
+import random, datetime, time
 
 
 # Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(max_length=30, verbose_name="昵称")
     email = models.EmailField(max_length=150, verbose_name="邮箱")
-    mobile = models.CharField(max_length=11)
+    mobile = models.CharField(max_length=11, verbose_name="手机号码")
+    city = models.CharField(max_length=20, null=True, verbose_name="城市")
     is_active = models.BooleanField(default=True, verbose_name="活跃状态")
-    birthday = models.DateField(verbose_name="生日", null=True)  #存储 date()
-    sex = models.CharField(max_length=5, choices=(('0', "男"), ("1", "女")), default=random.choice(['0', '1']))
+    birthday = models.DateField(verbose_name="生日", null=True)  # 存储 date()
+    sex = models.CharField(max_length=5, choices=(('0', "男"), ("1", "女")), default=random.choice(['0', '1']),
+                           verbose_name="性别")
     create_time = models.TimeField(auto_now_add=True, verbose_name="创建时间")  # 存储time.time()
     update_time = models.TimeField(auto_now=True, verbose_name="更新时间")  # 每次save操作自动保存
+    # 设置五张默认头像
     avatar = models.ImageField(upload_to="image/%Y/%m",
-                               default=u"image/default_{}.png".format(random.choice(range(1, 6))), max_length=100)
+                               default="image/default/{}.png".format(random.choice(range(1, 6))), max_length=100,
+                               verbose_name="头像")
     # 学院 一对多
 
     def __str__(self):
@@ -28,6 +32,45 @@ class User(AbstractUser):
             return -1  # 未知年龄
 
     class Meta:
+        db_table = "user"
         ordering = ["-create_time"]  # 新建用户在前
         verbose_name = "用户"
         verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    """首页轮播图, 与其他model很少产生关系"""
+    title = models.CharField(max_length=30, verbose_name="标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name="图片")
+    url = models.URLField(max_length=200, verbose_name="图片地址")
+    index = models.IntegerField(default=100, verbose_name="图片顺序")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+
+    class Meta:
+        db_table = "banner"
+        verbose_name = "首页轮播图"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "<{}: {}>".format(self.index, self.title)
+
+
+class EmailVerifyCode(models.Model):
+    """邮箱验证码"""
+    code = models.CharField(max_length=20, verbose_name="验证码")
+    email = models.CharField(max_length=30, verbose_name="邮箱")
+    send_time = models.TimeField(auto_now_add=True, verbose_name="发送时间")
+    send_type = models.CharField(max_length=10, choices=(("register", "注册"), ("forget", "找回密码")))
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+    def is_active(self):
+        if time.time() - self.send_time > 60:  # 一分钟过期
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return "<{}  {}: {}>".format(self.send_time, self.email, self.code)
