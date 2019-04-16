f3af397e8f8e2107956cbef17c56e759480c31ed
黎义 <liyi@forchange.tech>
2019-04-17 02:46:28 +0800

    fix: 对xadmin源码进行了修改, 使用requests以适配https请求

--- xadmin/plugins/themes.py
+++ xadmin/plugins/themes.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 from __future__ import print_function
 import httplib2
+import requests
 from django.template import loader
 from django.core.cache import cache
 from django.utils import six
@@ -71,12 +72,21 @@ class ThemePlugin(BaseAdminPlugin):
             else:
                 ex_themes = []
                 try:
-                    h = httplib2.Http()
-                    resp, content = h.request("https://bootswatch.com/api/3.json", 'GET', '',
-                                              headers={"Accept": "application/json", "User-Agent": self.request.META['HTTP_USER_AGENT']})
-                    if six.PY3:
-                        content = content.decode()
-                    watch_themes = json.loads(content)['themes']
+                    flag = False  # 假如为True使用原来的代码，假如为Flase，使用requests库来访问
+                    if flag:
+                        h = httplib2.Http()
+                        resp, content = h.request("https://bootswatch.com/api/3.json", 'GET', '',
+                                                  headers={"Accept": "application/json",
+                                                           "User-Agent": self.request.META['HTTP_USER_AGENT']})
+                        if six.PY3:
+                            content = content.decode()
+                        watch_themes = json.loads(content)['themes']
+                    else:
+                        content = requests.get("https://bootswatch.com/api/3.json")
+                        if six.PY3:
+                            content = content.text
+                        watch_themes = json.loads(content)['themes']
+
                     ex_themes.extend([
                         {'name': t['name'], 'description': t['description'],
                             'css': t['cssMin'], 'thumbnail': t['thumbnail']}
